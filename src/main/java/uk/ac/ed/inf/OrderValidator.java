4ff8783650be9ff63e748051ec3b243937e8dd8d
weijuinlee <weijuinlee@outlook.com>
2023-10-10 00:04:09 +0100

    added lng, lat handler, order validator and test

--- src/main/java/uk/ac/ed/inf/OrderValidator.java
+++ src/main/java/uk/ac/ed/inf/OrderValidator.java
@@ -3,13 +3,134 @@ package uk.ac.ed.inf;
 import uk.ac.ed.inf.ilp.data.Order;
 import uk.ac.ed.inf.ilp.data.Restaurant;
 import uk.ac.ed.inf.ilp.interfaces.OrderValidation;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-/**
- * a sample order validator which does nothing
- */
+import static uk.ac.ed.inf.ilp.constant.OrderValidationCode.*;
+
 public class OrderValidator implements OrderValidation {
+
     @Override
     public Order validateOrder(Order orderToValidate, Restaurant[] definedRestaurants) {
-        return null;
+
+        String dateString = String.valueOf(orderToValidate.getOrderDate());
+        isCreditCardValid(orderToValidate);
+
+        return orderToValidate;
     }
+
+    /**
+     * Checks if credit card is valid
+     * @param orderToValidate Order to be checked.
+     * Sets validation code accordingly
+     */
+    private static void isCreditCardValid(Order orderToValidate) {
+
+        System.out.println(isCardNumberValid(orderToValidate.getCreditCardInformation().getCreditCardNumber()));
+
+        if (!isCardNumberValid(orderToValidate.getCreditCardInformation().getCreditCardNumber())) {
+            orderToValidate.setOrderValidationCode(CARD_NUMBER_INVALID);
+        } else if (!isCVVValid(orderToValidate.getCreditCardInformation().getCvv())) {
+            orderToValidate.setOrderValidationCode(CVV_INVALID);
+        } else if (!isExpiryDateValid(orderToValidate.getCreditCardInformation().getCreditCardExpiry())) {
+            orderToValidate.setOrderValidationCode(EXPIRY_DATE_INVALID);
+        } else
+            orderToValidate.setOrderValidationCode(NO_ERROR);
+    }
+
+    /**
+     * Checks if cvv number is valid
+     * @param expiryDate String to be checked.
+     * @return True if string is numeric, false otherwise.
+     */
+    private static boolean isExpiryDateValid(String expiryDate) {
+
+        // Define the expected credit card date format
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/yy");
+        dateFormat.setLenient(false);
+
+        // Get the current date
+        Date currentDate = new Date();
+
+        try {
+            // Parse the expiry date
+            Date parsedExpiryDate = dateFormat.parse(expiryDate);
+
+            // Check if the parsed date is not in the past
+            return !parsedExpiryDate.before(currentDate);
+
+        } catch (ParseException e) {
+            // If parsing fails, the date format is invalid
+            return false;
+        }
+    }
+
+    /**
+     * Checks if cvv number is valid
+     * @param CVV String to be checked.
+     * @return True if string is numeric, false otherwise.
+     */
+    private static boolean isCVVValid(String CVV) {
+
+        // Clean the input by removing non-digit characters
+        String cleanedCVV = CVV.replaceAll("\\D", "");
+
+        // Check if the cleaned number has exactly 3 digits
+        return cleanedCVV.length() == 3;
+    }
+
+    /**
+     * Checks if credit card number is valid
+     * @param creditCardNumber String to be checked.
+     * @return True if string is numeric, false otherwise.
+     */
+    private static boolean isCardNumberValid(String creditCardNumber) {
+
+        // Clean the input by removing non-digit characters
+        String cleanedNumber = creditCardNumber.replaceAll("\\D", "");
+
+        // Check if the cleaned number has exactly 16 digits
+        if (cleanedNumber.length() == 16) {
+
+            // Check if the cleaned number is a valid credit card number using the Luhn algorithm
+            return isValidLuhn(cleanedNumber);
+
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Luhn algorithm for validating credit card number. The implementation is taken from the source below.
+     * https://www.geeksforgeeks.org/luhn-algorithm/
+     * @return True if valid, otherwise false.
+     */
+    private static boolean isValidLuhn(String number) {
+
+        int nDigits = number.length();
+
+        int nSum = 0;
+        boolean isSecond = false;
+        for (int i = nDigits - 1; i >= 0; i--)
+        {
+
+            int d = number.charAt(i) - '0';
+
+            if (isSecond)
+                d = d * 2;
+
+            // We add two digits to handle
+            // cases that make two digits
+            // after doubling
+            nSum += d / 10;
+            nSum += d % 10;
+
+            isSecond = !isSecond;
+        }
+        return (nSum % 10 == 0);
+    }
+
+
 }
+
---
+++ src/test/java/uk/ac/ed/inf/OrderValidatorTest.java
@@ -0,0 +1,55 @@
+package uk.ac.ed.inf;
+
+import uk.ac.ed.inf.ilp.constant.OrderStatus;
+import uk.ac.ed.inf.ilp.constant.OrderValidationCode;
+import uk.ac.ed.inf.ilp.constant.SystemConstants;
+import uk.ac.ed.inf.ilp.data.*;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class OrderValidatorTest {
+    public static void main(String[] args) {
+        System.out.println("ILP Test Application using the IlpDataObjects.jar file");
+
+        var order = new Order();
+        order.setOrderNo(String.format("%08X", ThreadLocalRandom.current().nextInt(1, Integer.MAX_VALUE)));
+        order.setOrderDate(LocalDate.of(2023, 9, 1));
+
+        order.setCreditCardInformation(
+                new CreditCardInformation(
+                        "5167679223795831",
+                        String.format("%02d/%02d", ThreadLocalRandom.current().nextInt(1, 12), ThreadLocalRandom.current().nextInt(24, 29)),
+                        "222"
+                )
+        );
+
+        // every order has the defined outcome
+        order.setOrderStatus(OrderStatus.UNDEFINED);
+        order.setOrderValidationCode(OrderValidationCode.UNDEFINED);
+
+        // get a random restaurant
+
+        // and load the order items plus the price
+        order.setPizzasInOrder(new Pizza[]{new Pizza("A", 1212)});
+        order.setPriceTotalInPence(1212 + SystemConstants.ORDER_CHARGE_IN_PENCE);
+
+        var validatedOrder =
+                new OrderValidator().validateOrder(order,
+                        new Restaurant[]{new Restaurant("myRestaurant",
+                                new LngLat(55.945535152517735, -3.1912869215011597),
+                                new DayOfWeek[]{DayOfWeek.MONDAY, DayOfWeek.FRIDAY},
+                                new Pizza[]{new Pizza("Pizza A", 2300)})
+                        });
+
+        if (validatedOrder != null){
+            System.out.println("order validation resulted in status: " +
+                    validatedOrder.getOrderStatus() +
+                    " and validation code: " +
+                    validatedOrder.getOrderValidationCode());
+        } else {
+            System.out.println("no order validated");
+        }
+    }
+}
