0760456008ced7a25e3a512c488e0ea4dbb5334a
weijuinlee <weijuinlee@outlook.com>
2023-11-13 00:10:31 +0000

    added check for argument length

--- src/main/java/uk/ac/ed/inf/App.java
+++ src/main/java/uk/ac/ed/inf/App.java
@@ -1,14 +1,14 @@
 package uk.ac.ed.inf;
 
-import uk.ac.ed.inf.ilp.constant.OrderStatus;
-
 public class App
 {
     public static void main( String[] args )
     {
         System.out.println("Small test for IlpDataObjects!");
 
-        var orderStatus = OrderStatus.DELIVERED;
+        if (args.length < 2) {
+            System.err.println("Invalid number of arguments in input, please input date and then url");
+        }
 
         System.out.println("IlpDataObjects.jar used");
     }
