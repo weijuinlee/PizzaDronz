59d1fc90f1593714ab51a8f1056c3abab9f82b5f
weijuinlee <weijuinlee@outlook.com>
2023-11-13 00:23:49 +0000

    added gitignore

--- src/main/java/uk/ac/ed/inf/App.java
+++ src/main/java/uk/ac/ed/inf/App.java
@@ -1,15 +1,23 @@
 package uk.ac.ed.inf;
 
+/**
+ * Main class for the pizzaDronz, this is the entry point of the application.
+ */
 public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Small test for IlpDataObjects!");
 
         if (args.length < 2) {
-            System.err.println("Invalid number of arguments in input, please input date and then url");
-        }
+            System.err.println("Invalid number of arguments in input, please input date and then url.");
+        } else {
 
-        System.out.println("IlpDataObjects.jar used");
+            // parsing arguments into date and url
+            String date = args[0];
+            String url = args[1];
+
+            System.out.println(date);
+            System.out.println(url);
+        }
     }
 }
