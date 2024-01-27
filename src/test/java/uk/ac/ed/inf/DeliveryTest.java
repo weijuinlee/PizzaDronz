7de379445e0502b988ff6e05489b16b88ef17caa
weijuinlee <weijuinlee@outlook.com>
2024-01-27 16:13:24 +0000

    added unit test for delivery

--- src/test/java/uk/ac/ed/inf/DeliveryTest.java
+++ src/test/java/uk/ac/ed/inf/DeliveryTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import uk.ac.ed.inf.ilp.constant.OrderStatus;
 import uk.ac.ed.inf.ilp.constant.OrderValidationCode;
 import uk.ac.ed.inf.ilp.data.Order;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,7 +17,6 @@ public class DeliveryTest {
 
     @Test
     public void testWriteToFile() throws IOException {
-
         // Prepare test data
         List<Order> orders = new ArrayList<>();
         Order order1 = new Order();
@@ -28,13 +26,7 @@ public class DeliveryTest {
         order1.setPriceTotalInPence(1000);
         orders.add(order1);
 
-        Order order2 = new Order();
-        order2.setOrderNo("12345678");
-        order2.setOrderStatus(OrderStatus.DELIVERED);
-        order2.setOrderValidationCode(OrderValidationCode.NO_ERROR);
-        order2.setPriceTotalInPence(1000);
-        orders.add(order2);
-
+        // Add more orders if necessary
         String date = "2023-11-11";
 
         // Call the method
@@ -42,14 +34,16 @@ public class DeliveryTest {
 
         // Verify that the JSON file is created
         String fileName = "deliveries-" + date + ".json";
-        Path filePath = Path.of("resultfiles", fileName);
+        Path directoryPath = Path.of("resultfiles");
+        Path filePath = directoryPath.resolve(fileName);
+
         assertTrue(Files.exists(filePath));
 
         // Verify the content of the JSON file
         String jsonContent = Files.readString(filePath);
         assertEquals("[", jsonContent.substring(0, 1)); // JSON array starts
         assertEquals("]", jsonContent.substring(jsonContent.length() - 1)); // JSON array ends
-        assertEquals(2, jsonContent.chars().filter(ch -> ch == '{').count()); // Two JSON objects for two orders
+        assertEquals(1, jsonContent.chars().filter(ch -> ch == '{').count()); // One JSON object for one order
 
         // Clean up: delete the created file
         Files.deleteIfExists(filePath);
